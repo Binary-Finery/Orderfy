c1856002a5b3d3a5e50c8267091d1a32d4a65dad
Dean Spencer <deanspencer007@gmail.com>
2019-10-18 20:55:02 +0100

    mods & additional functionality

---
+++ app/src/main/java/com/spencerstudios/orderfy/constants/Const.kt
@@ -0,0 +1,8 @@
+package com.spencerstudios.orderfy.constants
+
+class Const {
+    companion object {
+        const val IS_NEW_NOTE_KEY = "is_new_note"
+        const val EMPTY_NOTE_RECYCLER_VIEW_ITEM_TITLE = "[empty note]"
+    }
+}
